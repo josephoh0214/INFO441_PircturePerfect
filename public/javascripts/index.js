2c7a5c3552a63c7d23cee63d9e93fb2862a02763
josephoh0214 <josephoh0214@gmail.com>
2021-12-08 17:26:42 -0800

    modified function to display search result and added front-end accordingly

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,9 +1,17 @@
 async function getImages() {
     let query = document.getElementById("queryInput").value;
+
+    let h2 = document.getElementById("search_query");
+    query = query.substr(0, 1).toUpperCase() + query.substr(1, query.length);
+    h2.innerHTML = `${query}`;
+
     const images = await getImagesApi(query);
-    let h2 = document.getElementById("search_query").innerHTML;
-    h2.innerHTML = `<div>${query}</div>`;
-    console.log(query)
+    
+    let html = '';
+    for (let i = 0; i < images.length; i++) {
+      html += `<a href="${images[i].url}"><img src="${images[i].preview}"></img></a>`
+    }
+    document.getElementById("search_results").innerHTML = html;
 }
 
 async function init() {
